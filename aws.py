86d4deb391dd0a4fed6b2aa56f570b4653bc86b8
Serge Masyn <smasyn@yahoo.com>
2024-12-09 09:32:53 +0100

    change_language function

--- aws.py
+++ aws.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 app = Flask(__name__)
 
 # test
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def hello_world():
    return render_template("index_navbar.html")
    #return "Hello, World!"
+
+@app.route('/change-language/<lang>')
+def change_language(lang):
+    print(f"language selected: {lang}")
+
+    return redirect(url_for('index_navbar'))   
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
